b359ddffe749933daf18e2dffebb25ff5ba0d428
cH000k <noelcedrix@yahoo.com>
2023-05-07 23:48:46 +0800

    learn

--- hobby_showcase/app.js
+++ hobby_showcase/app.js
@@ -1,1 +1,16 @@
-let arr = ['noel', 'cedrix']
+const btn = document.querySelector('#submit');
+const ul = document.querySelector('#my-list');
+const input = document.querySelector('input');
+
+// btn.addEventListener("click", (e) => {
+//     const newLi = document.createElement('li');
+//     newLi.innerText = input.innerText;
+//     ul.appendChild(newLi)
+//     e.preventDefault();
+// })
+
+btn.addEventListener("click", (e) => {
+    
+    console.log(input.innerText);
+    e.preventDefault();
+})
