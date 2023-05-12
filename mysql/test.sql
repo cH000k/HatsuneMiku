98230e3e7a6c33d5e6598b6f0afa43f32a72624a
cH000k <noelcedrix@yahoo.com>
2023-05-12 18:11:42 +0800

    delete

---
+++ mysql/test.sql
@@ -0,0 +1,14 @@
+-- CREATE DATABASE sampledb; 
+
+-- CREATE TABLE sampledb.Movie (
+--     Title varchar(255), 
+--     YearCreate int, 
+--     score int
+-- );
+
+
+-- INSERT INTO sampledb.Movie VALUES
+-- ('Monty', 1975, 8.2),
+-- ('Now for Something', 1971, 7.5)
+
+SELECT * FROM sampledb.Movie
